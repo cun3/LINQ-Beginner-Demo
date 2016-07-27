b1d3c021ac0b418f8bbc30d005170e2abf089f1a
cun3 <nachiketh1989@gmail.com>
2016-07-26 23:02:03 -0400

    Added Demo4

---
+++ LinqQuerySnippets/LinqQuerySnippets/Demo4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqQuerySnippets
+{
+    class Demo4
+    {
+        static void Main(string[] args)
+        {
+            //OfType Example
+            object[] array = new object[4];
+            array[0] = new StringBuilder();
+            array[1] = "example";
+            array[2] = new int[1];
+            array[3] = "another";
+
+            // Print only the strings to the screen.
+            var result = array.OfType<string>();
+            foreach (var element in result)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
